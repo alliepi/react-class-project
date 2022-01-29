72ad4ee33be3f0e27fb4e60a58ca5e015d3a295d
Allie Pistolessi <a.pistol17@gmail.com>
2022-01-28 20:08:45 -0500

    React Router Parameters

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -35,7 +35,7 @@ import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reac
                 <div className="container">
                     <div className="row">
                         <RenderCampsite campsite={props.campsite} />
-                        <RenderComments comments={props.campsite.comments} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
             )
