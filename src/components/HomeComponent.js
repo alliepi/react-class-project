625338787de3d78ba89bddbadbf5fe4067aae767
Allie Pistolessi <a.pistol17@gmail.com>
2022-02-08 06:18:10 -0500

    Redux Thunk

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import {Loading} from './LoadingComponent';
 
-function RenderCard({item}){
+function RenderCard({item, isLoading, errMess}){
+    if (isLoading){
+        return <Loading />
+    }
+    if (errMess){
+        return <h4>{errMess}</h4>
+    }
     return (
         <Card>
             <CardImg src={item.image} alt={item.name} />
@@ -21,10 +28,14 @@ function Home(props) {
                     <RenderCard item={props.campsite} />
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.promotion} />
+                    <RenderCard 
+                        item={props.campsite} 
+                        isLoading={props.campsitesLoading}
+                        errMess={props.campsitesErrMess}
+                        />
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.partner} />
+                    <RenderCard item={props.campsite} />
                 </div>
             </div>
         </div>
