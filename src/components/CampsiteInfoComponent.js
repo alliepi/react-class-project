234912dbd79c65e8da28217136f6724144279ed2
Allie Pistolessi <a.pistol17@gmail.com>
2022-02-10 22:00:17 -0500

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -61,7 +61,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text)
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text)
   }
 
   render() {
@@ -139,7 +139,7 @@ class CommentForm extends Component {
 }
 
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -158,7 +158,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
             </p>
           </div>
         ))}
-        <CommentForm campsiteId={campsiteId} addComment={addComment} />
+        <CommentForm campsiteId={campsiteId} postComment={postComment} />
       </div>
     );
   }
@@ -205,7 +205,7 @@ function CampsiteInfo(props) {
           <RenderCampsite campsite={props.campsite} />
           <RenderComments 
             comments={props.comments} 
-            addComment={props.addComment}
+            postComment={props.postComment}
             campsiteId={props.campsite.id}
             />
         </div>
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,15 +1,7 @@
 import * as ActionTypes from './ActionTypes';
-import {baseUrl} from "../shared/baseUrl";
+import {baseUrl} from '../shared/baseUrl';
 
-export const addComment = (campsiteId, rating, author, text) => ({
-    type: ActionTypes.ADD_COMMENT,
-    payload: {
-        campsiteId,
-        rating,
-        author,
-        text
-    }
-})
+
 
 //simulates call to server by creating a delay
 export const fetchCampsites = () => dispatch => {
@@ -49,6 +41,51 @@ export const addCampsites = campsites => ({
     payload: campsites
 });
 
+export const addComments = comments => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
+export const addComment = comment => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
+});
+
+export const postComment = (campsiteId, rating, author, text) => dispatch => {
+    const newComment = {
+        campsiteId: campsiteId,
+        rating: rating,
+        author: author,
+        text: text
+    }
+    newComment.date = new Date().toISOString();
+
+    return fetch(baseUrl + 'comments', {
+        method: "POST", 
+        body: JSON.stringify(newComment), 
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => {throw error; }
+    )
+    .then(response => response.json())
+    .then(response => dispatch(addComment(response)))
+    .catch(error=> {
+        console.log('post comment', error.message);
+        alert('Your comment could not be posted\nError: ' + error.message);
+    })
+}
+
 export const fetchComments = () => dispatch => {    
     return fetch(baseUrl + 'comments')
         .then(response => {
@@ -75,10 +112,7 @@ export const commentsFailed = errMess => ({
     payload: errMess
 });
 
-export const addComments = comments => ({
-    type: ActionTypes.ADD_COMMENTS,
-    payload: comments
-});
+
 
  export const fetchPromotions = () => dispatch => {
     dispatch(promotionsLoading());
